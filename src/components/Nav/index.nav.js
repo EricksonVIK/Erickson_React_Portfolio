1ff9dbfc6abf58727771bdd98956e55cb0abf87e
Erikson <chris.s.erickson@gmail.com>
2022-10-31 08:28:39 -0400

    added resume and project tab

--- src/components/Nav/index.nav.js
+++ src/components/Nav/index.nav.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
 
 function Nav() {
+  const pages = [
+    {
+      name: "projects",
+      description: "Selection of projects",
+    },
+    {
+      name: "resume",
+      description: "Resume page",
+    },
+  ];
 
   return (
-      <header>
-          <h1>Testing Nav</h1>
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+          </span>{" "}
+          Erickson
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {pages.map((page) => (
+            <li className="mx-2" key={page.name}>
+              <span> {page.name} </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
