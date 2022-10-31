8f455e700d98380f7ebebf31cf49e82552ef228f
Erikson <chris.s.erickson@gmail.com>
2022-10-31 08:06:35 -0400

    About

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+// Root Component for Application
 import React, { useState } from "react";
+import Nav from "./components/Nav/index.nav"
 import About from "./components/About/index.about";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* <Nav></Nav> */}
       <main>
         <About></About>
       </main>
